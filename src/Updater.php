a5616ad05ae186bc766f07de9a0b7337ab23160b
Artem Vasilev <kern.usr@gmail.com>
2022-11-23 15:58:59 +0300

    Ещё немного исправлений в апдейтере

--- src/Updater.php
+++ src/Updater.php
@@ -2,22 +2,18 @@
 
 namespace Webmasterskaya\ProductionCalendar;
 
-use DOMDocument;
-use DOMElement;
-use DOMXPath;
-
-/**
- * Class Updater
- *
- * @package webmasterskaya\ProductionCalendar
- */
 class Updater
 {
 	protected const OUTPUT_PATH = 'data/holidays.json';
 	protected const BACKUP_PATH = 'data/backup.holidays.json';
 
+	/**
+	 * @throws \Exception
+	 */
 	public static function execute($arg = null)
 	{
+		/** @noinspection PhpUndefinedNamespaceInspection */
+		/** @noinspection PhpUndefinedClassInspection */
 		if ($arg instanceof \Composer\Script\Event)
 		{
 			$args = $arg->getArguments();
@@ -37,11 +33,20 @@ class Updater
 		}
 	}
 
+	/**
+	 * @throws \Exception
+	 */
 	public function __invoke($arg = null)
 	{
 		static::execute($arg);
 	}
 
+	/**
+	 * Обновляет справочник дат за весь доступный период (2013 - текущий год и следующий, если опубликован)
+	 *
+	 * @throws \Exception
+	 * @return void
+	 */
 	public static function updateAll()
 	{
 		$output = dirname(__FILE__) . '/' . ltrim(static::OUTPUT_PATH, '/');
@@ -78,14 +83,24 @@ class Updater
 			{
 				copy($backup, $output);
 			}
-		}
 
-		if (file_exists($backup))
+			throw $e;
+		} finally
 		{
-			unlink($backup);
+			if (file_exists($backup))
+			{
+				unlink($backup);
+			}
 		}
 	}
 
+	/**
+	 * Обновляет справочник дат за указанный год
+	 *
+	 * @param   int|string  $year  Год, за который нужно получить справочник. null - екущий год
+	 *
+	 * @throws \Exception
+	 */
 	public static function update($year = null)
 	{
 		if (empty($year))
@@ -113,14 +128,14 @@ class Updater
 			throw new \Exception(curl_error($ch), curl_errno($ch));
 		}
 
-		$document = new DomDocument();
+		$document = new \DOMDocument();
 
 		// Отключает ошибки парсинга HTML5 элементов
 		libxml_use_internal_errors(true);
 		$document->loadHTML($result);
 		libxml_use_internal_errors(false);
 
-		$xpath = new DomXPath($document);
+		$xpath = new \DOMXPath($document);
 
 		$tables_nodes = $xpath->query("//*/table[contains(concat(' ', normalize-space(@class), ' '), cal)]");
 
@@ -129,7 +144,7 @@ class Updater
 			$dates = json_decode(file_get_contents($output), true, 128, JSON_OBJECT_AS_ARRAY);
 			if (json_last_error() !== JSON_ERROR_NONE)
 			{
-				throw new \Exception(json_last_error_msg(), 'error');
+				throw new \Exception(json_last_error_msg(), 1);
 			}
 		}
 		else
@@ -147,7 +162,7 @@ class Updater
 
 		$m = 0;
 
-		/** @var DOMElement $table_node */
+		/** @var \DOMElement $table_node */
 		foreach ($tables_nodes as $table_node)
 		{
 			// Дополнительная проверка на косяки, при выборке таблиц по классу
@@ -160,11 +175,11 @@ class Updater
 			$tds_nodes = $table_node->getElementsByTagName('td');
 			$month     = str_pad($m, 2, '0', STR_PAD_LEFT);
 
-			/** @var DOMElement $td_node */
+			/** @var \DOMElement $td_node */
 			foreach ($tds_nodes as $td_node)
 			{
 				$day = str_pad(
-					preg_replace('/[\D]/', '', $td_node->textContent), 2,
+					preg_replace('/\D/', '', $td_node->textContent), 2,
 					'0',
 					STR_PAD_LEFT
 				);
@@ -214,7 +229,7 @@ class Updater
 
 			if (json_last_error() !== JSON_ERROR_NONE)
 			{
-				throw new \Exception(json_last_error_msg(), 'error');
+				throw new \Exception(json_last_error_msg(), 1);
 			}
 
 			file_put_contents($output, $dates_json);
