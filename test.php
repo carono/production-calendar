809999c75a2af692f772b94732b654e09803ae07
Александр Касьянов <info@carono.ru>
2024-01-08 14:53:50 +0500

    Добавлен 2024 год

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
-require 'ProductionCalendar.php';
+require 'Calendar.php';
+
 use carono\production\Calendar;
 
-$expect = [true, false, '2016-05-10', true, true, true];
+$expect = [true, false, '2016-05-10', true, true, true, true, '2024-01-09'];
 $res = [];
 $res['2016-05-09 is holiday'] = Calendar::isHoliday('2016-05-09'); // true
 $res['2016-05-09 is working'] = Calendar::isWorking('2016-05-09'); // false
@@ -10,6 +11,8 @@ $res['first working day after 2016-05-07'] = Calendar::find('2016-05-07')->worki
 $res['2016-02-20 is weekend'] = Calendar::isWeekend('2016-02-20'); // true
 $res['2016-02-20 is pre holiday'] = Calendar::isPreHoliday('2016-02-20'); // true
 $res['2017-01-01 is holiday'] = Calendar::find('2016-12-31')->next()->isHoliday(); //true
+$res['2024-01-01 is holiday'] = Calendar::find('2023-12-31')->next()->isHoliday(); //true
+$res['first working day after 2024-01-01'] = Calendar::find('2024-01-01')->working()->format(); //true
 
 print_r($res);
 foreach ($res as $result) {
